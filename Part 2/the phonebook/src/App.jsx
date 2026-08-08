80dc9b500a1f4bf0ee72dd12e6d4a8c0bd23ea3b
Kelvin Addy <Keladdy2131@gmail.com>
2026-08-08 01:33:39 +0000

    Moved the phonebook data to db.json

--- Part 2/the phonebook/src/App.jsx
+++ Part 2/the phonebook/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [personsFound, setPersonsFound] = useState([]);
   const [query, setQuery] = useState("");
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
