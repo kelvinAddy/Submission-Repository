bea1d485ddd93b758bc019529ec3e3a6683567f8
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-15 00:53:22 +0000

    Configured express application

---
+++ Part 4/BlogList/App/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const middleware = require('../App/Middlewares/middleware');
+const blogRouter = require('../App/Routes/blog.router');
+
+const app = express();
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/notes', blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
