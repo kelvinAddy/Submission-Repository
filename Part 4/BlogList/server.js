d7adac57690d0b834b17ced2c9e90e1395ebbc7f
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-15 01:03:01 +0000

    Configured server startup when connection to database is successful

--- Part 4/BlogList/server.js
+++ Part 4/BlogList/server.js
@@ -1,7 +1,13 @@
 const logger = require('./App/Utils/logger');
 const connectToDb = require('./App/Utils/db');
 const app = require('./App/app');
+const config = require('./App/Utils/config');
 
 connectToDb()
-  .then(() => logger.info('Connected to Database'))
+  .then(() => {
+    logger.info('Connected to Database');
+    app.listen(config.PORT, () => {
+      logger.info(`Server is running on PORT ${config.PORT}`);
+    });
+  })
   .catch((error) => logger.error('Error was encountered', error.message));
