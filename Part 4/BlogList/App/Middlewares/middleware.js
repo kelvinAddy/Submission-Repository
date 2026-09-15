858b6e7839f9f3285d08389832d9f5a816f4ab1d
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-15 00:34:19 +0000

    Configured middlewares for the application

---
+++ Part 4/BlogList/App/Middlewares/middleware.js
@@ -0,0 +1,22 @@
+const logger = require('../Utils/logger');
+
+const requestLogger = (req, res, next) => {
+  logger.info('METHOD :', req.method);
+  logger.info('PATH :', req.path);
+  logger.info('BODY :', req.body);
+  logger.info('----');
+  next();
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).json({ error: 'Unknown Endpoint' });
+};
+
+const errorHandler = (error, req, res, next) => {
+  if (error.message === 'CastError') {
+    res.status(500).json({ error: 'Malformed Id' });
+  } else if (error.message === 'ValidationError') {
+    res.status(400).json(error);
+  }
+  next(error);
+};
