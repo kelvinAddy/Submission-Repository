633ac380320aa39d20511833f207f1f51f0102ac
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-15 00:57:30 +0000

    Updated db.js with connecting to database logger

--- Part 4/BlogList/App/Utils/db.js
+++ Part 4/BlogList/App/Utils/db.js
@@ -1,7 +1,9 @@
 const config = require('./config');
+const logger = require('../Utils/logger');
 const mongoose = require('mongoose');
 
 const connectToDb = async () => {
+  logger.info('Connecting to the Database.....');
   mongoose.connect(config.MONGODB_URI, { family: 4 });
 };
 
