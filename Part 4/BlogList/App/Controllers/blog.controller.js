4d168c6cb5c95c25997afb853335a6ad58991346
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-17 17:55:31 +0000

    Refactored controllers, added aync await and added validation for blog data

--- Part 4/BlogList/App/Controllers/blog.controller.js
+++ Part 4/BlogList/App/Controllers/blog.controller.js
@@ -1,15 +1,22 @@
 const Blog = require('../Models/blog.model');
 
-exports.getBlogs = (req, res) => {
-  Blog.find({}).then((blogs) => {
-    res.json(blogs);
-  });
+exports.getBlogs = async (req, res) => {
+  const blogs = await Blog.find({});
+  res.json(blogs);
 };
 
-exports.postBlog = (req, res) => {
-  const blog = new Blog(req.body);
+exports.postBlog = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ error: 'Not a valid JSON' });
+  }
 
-  blog.save().then((result) => {
-    res.status(201).json(result);
+  if (!req.body.url || !req.body.title) {
+    return res.status(400).json({ error: 'Url or title is missing' });
+  }
+
+  const blog = await Blog.create({
+    ...req.body,
+    likes: req.body.likes ?? 0,
   });
+  res.status(201).json(blog);
 };
