7afb7ff16f1900ea87920cfb472c43a98d218b10
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-17 15:17:59 +0000

    Transform responses from the blog api to have unique identifier id instead of _id

--- Part 4/BlogList/App/Models/blog.model.js
+++ Part 4/BlogList/App/Models/blog.model.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const blogSchema = mongoose.Schema(
+  {
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+  },
+  {
+    toJSON: {
+      transform: (doc, resObj) => {
+        resObj.id = resObj._id.toString();
+        delete resObj._id;
+        delete resObj.__v;
+      },
+    },
+  },
+);
 
 const Blog = mongoose.model('Blog', blogSchema);
 
