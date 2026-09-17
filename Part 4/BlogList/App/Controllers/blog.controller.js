8f1addf1cb621fdef97cabc5c8ddbd75b1c5dda3
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-17 23:41:08 +0000

    Added controller for deleting blogs from the db

--- Part 4/BlogList/App/Controllers/blog.controller.js
+++ Part 4/BlogList/App/Controllers/blog.controller.js
@@ -7,7 +7,7 @@ exports.getBlogs = async (req, res) => {
 
 exports.postBlog = async (req, res) => {
   if (!req.body) {
-    return res.status(400).json({ error: 'Not a valid JSON' });
+    return res.status(400).json({ error: 'Data is invalid' });
   }
 
   if (!req.body.url || !req.body.title) {
@@ -19,4 +19,27 @@ exports.postBlog = async (req, res) => {
     likes: req.body.likes ?? 0,
   });
   res.status(201).json(blog);
+};
+
+exports.deleteBlog = async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+};
+
+exports.putBlog = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ error: 'Data is invalid' });
+  }
+
+  if (!req.body.url || !req.body.title) {
+    return res.status(400).json({ error: 'Url or title is missing' });
+  }
+
+  const fetchedBlog = await Blog.findById(req.params.id);
+  if (!fetchedBlog) {
+    return res.status(404).end();
+  }
+  fetchedBlog.likes = req.body.likes;
+  const savedBlog = await fetchedBlog.save();
+  res.json(savedBlog);
 };
