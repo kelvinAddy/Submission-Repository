65697da6c4f7cc774bd5f76298bcce315896e5c2
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-17 14:06:59 +0000

    Updated to connect to db in app instead of server when in test mode

--- Part 4/BlogList/App/app.js
+++ Part 4/BlogList/App/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const middleware = require('../App/Middlewares/middleware');
 const blogRouter = require('../App/Routes/blog.router');
+const connectToDb = require('../App/Utils/db');
 
 const app = express();
+
+process.env.NODE_ENV === 'test' && connectToDb();
 
 app.use(express.json());
 app.use(middleware.requestLogger);
