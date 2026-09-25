1f266c7be58a8b299cd168d2c7184d57d9b546a8
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-25 00:21:57 +0000

    Added router for login and users, as well as middleware for token extraction

--- Part 4/BlogList/App/app.js
+++ Part 4/BlogList/App/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const middleware = require('../App/Middlewares/middleware');
-const blogRouter = require('../App/Routes/blog.router');
 const connectToDb = require('../App/Utils/db');
+const blogRouter = require('../App/Routes/blog.router');
+const userRouter = require('./Routes/user.router');
+const loginRouter = require('./Routes/login.router');
 
 const app = express();
 
 process.env.NODE_ENV === 'test' && connectToDb();
 
 app.use(express.json());
+app.use(middleware.getToken);
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
