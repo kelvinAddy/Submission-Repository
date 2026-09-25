7f553a840872b0760ced9e37f19c8566e80b8432
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-25 00:30:31 +0000

    added user extractor for blog related crud operations

--- Part 4/BlogList/App/Routes/blog.router.js
+++ Part 4/BlogList/App/Routes/blog.router.js
@@ -1,7 +1,11 @@
 const blogController = require('../Controllers/blog.controller');
+const userExtractor = require('../Middlewares/middleware').userExtractor;
 const router = require('express').Router();
 
 router.get('/', blogController.getBlogs);
+
+router.use(userExtractor);
+
 router.post('/', blogController.postBlog);
 router.delete('/:id', blogController.deleteBlog);
 router.put('/:id', blogController.putBlog);
