f258f46f1de2e992725e1ac13b0f05df355ae52e
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-25 00:28:28 +0000

    Refactored error handler and added handlers for token and user extraction

--- Part 4/BlogList/App/Middlewares/middleware.js
+++ Part 4/BlogList/App/Middlewares/middleware.js
@@ -1,4 +1,28 @@
 const logger = require('../Utils/logger');
+const User = require('../Models/user.model');
+const jwt = require('jsonwebtoken');
+
+const getToken = (req, res, next) => {
+  const authorization = req.get('authorization');
+  req.token = null;
+  if (authorization && authorization.startsWith('Bearer')) {
+    req.token = authorization.replace('Bearer ', '');
+  }
+
+  next();
+};
+
+const userExtractor = async (req, res, next) => {
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'Token is invalid' });
+  }
+  const fetchedUser = await User.findById(decodedToken.id);
+  req.user = fetchedUser;
+
+  next();
+};
 
 const requestLogger = (req, res, next) => {
   logger.info('METHOD :', req.method);
@@ -13,12 +37,32 @@ const unknownEndpoint = (req, res) => {
 };
 
 const errorHandler = (error, req, res, next) => {
-  if (error.message === 'CastError') {
-    res.status(500).json({ error: 'Malformed Id' });
-  } else if (error.message === 'ValidationError') {
-    res.status(400).json(error);
+  switch (true) {
+    case error.name === 'CastError':
+      return res.status(500).json({ error: 'Malformed Id' });
+
+    case error.name === 'ValidationError':
+      return res.status(400).json(error.message);
+
+    case error.name === 'MongoServerError' &&
+      error.message.includes('E11000 duplicate key error'):
+      return res.status(400).json({ error: 'username must be unique' });
+
+    case error.name === 'JsonWebTokenError': {
+      return res.status(401).json({ error: 'token is missing or invalid' });
+    }
+    case error.name === 'TokenExpiredError':
+      return res.status(401).json({ error: 'token expired' });
+
+    default:
+      next(error);
   }
-  next(error);
 };
 
-module.exports = { unknownEndpoint, errorHandler, requestLogger };
+module.exports = {
+  getToken,
+  userExtractor,
+  unknownEndpoint,
+  errorHandler,
+  requestLogger,
+};
