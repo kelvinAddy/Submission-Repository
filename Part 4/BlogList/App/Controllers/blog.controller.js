2dbe6d789db43b4698e470e250c3baedc7d61021
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-25 00:25:42 +0000

    implemented authorization for blog crud actions

--- Part 4/BlogList/App/Controllers/blog.controller.js
+++ Part 4/BlogList/App/Controllers/blog.controller.js
@@ -1,45 +1,57 @@
 const Blog = require('../Models/blog.model');
+const User = require('../Models/user.model');
 
 exports.getBlogs = async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, _id: 1 });
   res.json(blogs);
 };
 
 exports.postBlog = async (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({ error: 'Data is invalid' });
-  }
-
-  if (!req.body.url || !req.body.title) {
+  if (!req?.body?.url || !req?.body?.title) {
     return res.status(400).json({ error: 'Url or title is missing' });
   }
 
+  const user = req.user;
+
   const blog = await Blog.create({
-    ...req.body,
-    likes: req.body.likes ?? 0,
+    ...req?.body,
+    likes: req?.body?.likes ?? 0,
+    user: user._id,
   });
+
+  user.blogs = [...user.blogs, blog._id];
+  await user.save();
+
   res.status(201).json(blog);
 };
 
 exports.deleteBlog = async (req, res) => {
-  await Blog.findByIdAndDelete(req.params.id);
-  res.status(204).end();
+  const user = req.user;
+
+  const blogToDelete = await Blog.findById(req.params.id);
+
+  if (blogToDelete?.user?.toString() === user._id.toString()) {
+    await blogToDelete.deleteOne();
+    return res.status(204).end();
+  } else {
+    res.status(401).json({ error: 'Unable to perform the request' });
+  }
 };
 
 exports.putBlog = async (req, res) => {
-  if (!req.body) {
-    return res.status(400).json({ error: 'Data is invalid' });
-  }
-
-  if (!req.body.url || !req.body.title) {
+  if (!req?.body?.url || !req?.body?.title) {
     return res.status(400).json({ error: 'Url or title is missing' });
   }
 
-  const fetchedBlog = await Blog.findById(req.params.id);
+  const user = req.user;
+  const fetchedBlog = await Blog.findById(req?.params?.id);
+
+  if (user._id.toString() === fetchedBlog.user.toString()) {
+    fetchedBlog.likes = req.body.likes;
+    const savedBlog = await fetchedBlog.save();
+    return res.json(savedBlog);
+  }
   if (!fetchedBlog) {
     return res.status(404).end();
   }
-  fetchedBlog.likes = req.body.likes;
-  const savedBlog = await fetchedBlog.save();
-  res.json(savedBlog);
 };
